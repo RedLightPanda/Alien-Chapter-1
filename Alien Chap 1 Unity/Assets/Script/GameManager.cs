2d359ed4352e2920d0b8b7a09770df3f437923b2
RedLightPanda <56239248+RedLightPanda@users.noreply.github.com>
2020-10-05 11:42:32 -0700

    New work

--- Alien Chap 1 Unity/Assets/Script/GameManager.cs
+++ Alien Chap 1 Unity/Assets/Script/GameManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UI_Driver;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+    public static UI_Driver_System UI = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (instance == null)
+        if (instance && UI == null)
         {
             instance = this;
+            //UI = UI_Driver ;
             DontDestroyOnLoad(gameObject);
         }
         else
---
+++ Alien Chap 1 Unity/Assets/Script/Player/InShip_2DPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InShip_2DPlayer : MonoBehaviour
+{
+
+    #region Varialbes
+
+    [SerializeField]
+    private float _Speed = 2.5f;
+
+    private bool facingRight;
+
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingRight = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalaculateMovement();
+
+        Flip();
+    }
+
+    #region Helper Code
+    void CalaculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontalInput, 0);
+
+        transform.Translate(direction * _Speed * Time.deltaTime);
+    }
+
+    private float Flip (float horizontal)
+    {
+        if (horizontal >0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector2 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
+
+    #endregion
+}
