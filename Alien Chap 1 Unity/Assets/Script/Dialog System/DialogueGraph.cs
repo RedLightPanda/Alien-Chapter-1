d65aa5f12670b28567003bc4bb8c00aef9b39d0b
RedLightPanda <56239248+RedLightPanda@users.noreply.github.com>
2020-10-11 02:54:42 -0600

    Dialogue grpah stuck

--- Alien Chap 1 Unity/Assets/Script/Dialog System/DialogueGraph.cs
+++ Alien Chap 1 Unity/Assets/Script/Dialog System/DialogueGraph.cs
@@ -1,14 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DialogueGraph : EditorWindow
 {
+    private DialogueGraphView _graphView;
+    private string _fileName = "New Narrative";
+
+
     [MenuItem ("Graph/Dialogue Graph")]
     public static void OpenDialogueGraphWindow()
     {
         var window = GetWindow<DialogueGraph>();
         window.titleContent = new GUIContent(text:"Dialogue Graph");
     }
+
+    private void OnEnable()
+    {
+        ConstructGraphView();
+        GenerateToolbar();
+    }
+
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
+    }
+    #region Graph View
+    private void ConstructGraphView()
+    {
+        _graphView = new DialogueGraphView
+        {
+            name = "Dialogue Graph"
+        };
+
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+    }
+    #endregion
+
+    #region ToolBar
+    private void GenerateToolbar()
+    {
+        var toolbar = new Toolbar();
+
+        var fileNameTextField = new TextField(label: "File Name:");
+        fileNameTextField.SetValueWithoutNotify(_fileName);
+        fileNameTextField.MarkDirtyRepaint();
+        fileNameTextField.RegisterValueChangedCallback(EventType:ChangeEvent<string> => _fileName =EventType.newValue);
+        toolbar.Add(fileNameTextField);
+
+        var nodeCreateButton = new Button( clickEvent: () => { _graphView.CreateNode("Dialogue Node"); });
+        nodeCreateButton.text = "Creat Node";
+        toolbar.Add(nodeCreateButton);
+
+        rootVisualElement.Add(toolbar);
+    }
+    #endregion
 }
