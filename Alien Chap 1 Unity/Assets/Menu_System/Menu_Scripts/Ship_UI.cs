685186fc8d5ccd5febd91767649a38acd7260dc0
RedLightPanda <56239248+RedLightPanda@users.noreply.github.com>
2020-11-01 02:59:09 -0800

    work

---
+++ Alien Chap 1 Unity/Assets/Menu_System/Menu_Scripts/Ship_UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ship_UI : MonoBehaviour
+{
+    //HP Bar
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMaxHealth(int Health)
+    {
+        slider.maxValue = Health;
+        slider.value = Health;
+    }
+
+    public void SetHealth(int Health)
+    {
+        slider.value = Health;
+    }
+}
